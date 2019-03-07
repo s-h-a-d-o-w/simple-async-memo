540b695bcba9742c76675397e2ac7b406545ab01
Andreas Opferkuch <s-h-a-d-o-w@users.noreply.github.com>
2019-03-08 00:12:47 +0100

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,56 @@
 import {arrayIsEqual, findKeyIdx, CompareFn} from './utils';
 
 export type Options = {
-	maxAge?: number;
-	matchesKey?: CompareFn;
-	rejectRetryDelay?: number;
+	maxAge: number;
+	matchesKey: CompareFn;
+	rejectRetryDelay: number;
 };
+
+type Cache = {
+	keys: any[][];
+	values: any[];
+	expirations: number[];
+};
+
+function initCacheEntry(
+	fn: Function,
+	args: any[],
+	cache: Cache,
+	opts: Options
+) {
+	return fn
+		.apply(null, args)
+		.then(() => {
+			const innerIdx = findKeyIdx(cache.keys, args, opts.matchesKey);
+			cache.expirations[innerIdx] = Date.now() + opts.maxAge;
+		})
+		.catch((error: any) => {
+			// The initial rejection is passed along but leads to retries
+			// console.log('Initial call rejected, scheduling retry.');
+			const innerIdx = findKeyIdx(cache.keys, args, opts.matchesKey);
+			cache.expirations[innerIdx] = Date.now() + opts.rejectRetryDelay;
+			throw error;
+		});
+}
+
+function renewCacheEntry(
+	fn: Function,
+	args: any[],
+	cache: Cache,
+	opts: Options,
+	idx: number
+) {
+	fn.apply(null, args)
+		.then((retval: any) => {
+			// console.log('Cache result success');
+			cache.expirations[idx] = Date.now() + opts.maxAge;
+			cache.values[idx] = Promise.resolve(retval);
+		})
+		.catch(() => {
+			// console.log('Rejected, scheduling retry.');
+			cache.expirations[idx] = Date.now() + opts.rejectRetryDelay;
+		});
+}
 
 export const memoize = (fn: Function, opts?: Options) => {
 	const _opts = Object.assign(
@@ -17,52 +63,31 @@ export const memoize = (fn: Function, opts?: Options) => {
 		opts
 	);
 
-	const keyStore: any[] = [];
-	const valStore: any[] = [];
-	const timeoutStore: number[] = [];
+	const cache: Cache = {
+		keys: [],
+		values: [],
+		expirations: [],
+	};
 
 	const memoizedFn = (...args: any[]) => {
 		// console.log(`${fn.name}(${args})`);
-		const idx = findKeyIdx(keyStore, args, _opts.matchesKey);
+		const idx = findKeyIdx(cache.keys, args, _opts.matchesKey);
 		if (idx >= 0) {
-			// Renew expired cache in the background
-			if (Date.now() > timeoutStore[idx]) {
-				// console.log('Renew cache');
-				fn.apply(null, args)
-					.then((retval: any) => {
-						// console.log('Cache result success');
-						timeoutStore[idx] = Date.now() + _opts.maxAge;
-						valStore[idx] = Promise.resolve(retval);
-					})
-					.catch(() => {
-						// console.log('Rejected, scheduling retry.');
-						timeoutStore[idx] = Date.now() + _opts.rejectRetryDelay;
-					});
+			if (Date.now() > cache.expirations[idx]) {
+				// Renew expired cache in the background
+				renewCacheEntry(fn, args, cache, _opts, idx);
 			}
 
-			return valStore[idx];
+			return cache.values[idx];
 		} else {
-			const promise = fn
-				.apply(null, args)
-				.then(() => {
-					const innerIdx = findKeyIdx(keyStore, args, _opts.matchesKey);
-					timeoutStore[innerIdx] = Date.now() + _opts.maxAge;
-				})
-				.catch((error: any) => {
-					// The initial rejection is passed along but leads to retries
-					// console.log('Initial call rejected, scheduling retry.');
-					const innerIdx = findKeyIdx(keyStore, args, _opts.matchesKey);
-					timeoutStore[innerIdx] = Date.now() + _opts.rejectRetryDelay;
-					throw error;
-				});
+			const promise = initCacheEntry(fn, args, cache, _opts);
 
-			// Create temporary cache entry
-			// Necessary in case another call is made before promise resolves
-			keyStore.unshift(args);
-			timeoutStore.unshift(Number.MAX_SAFE_INTEGER);
-			valStore.unshift(promise);
+			// Create temporary cache entry (key/value may be permanent but expiration will change)
+			// Necessary in case another call is made before the Promise resolves
+			cache.keys.unshift(args);
+			cache.expirations.unshift(Number.MAX_SAFE_INTEGER);
+			cache.values.unshift(promise);
 
-			// console.log('Created new cache entry');
 			return promise;
 		}
 	};
